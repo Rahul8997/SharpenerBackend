37f97134fbd8d311f829d2020463f90b2502045c
Rahul8997 <rdprajapati8997@gmail.com>
2023-05-27 02:51:07 +0530

    Backend-Kickstarting Node Js-Task-8 added

---
+++ BackEnd-Kickstarting Node Js-Task-8/app.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log(req);
+    const url = req.url;
+
+    if (url === '/') {
+        fs.readFile('message.txt',{encoding:'utf8'},(err,data)=>{
+            if(err){
+                console.log(err);
+            }
+            console.log("data"+data);
+            res.write('<html>')
+            res.write('<head><title>Enter message</title></head>')
+            res.write(`<body>${data}</body>`)
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input><button type="submit">Send</button></form></body>')
+            res.write('</html>')
+            return res.end();
+        })
+    }
+    if (url === '/message' && req.method == "POST") {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+            console.log(chunk);
+        })
+
+        req.on('end',()=>{
+            const parseData=Buffer.concat(body).toString();
+            const message=parseData.split('=')[1];
+            fs.writeFileSync("message.txt",message);
+            res.statusCode = "302";
+            res.setHeader("Location", '/')
+            return res.end();
+        })
+    }
+})
+
+server.listen(4000);
