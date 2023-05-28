4ce7fb1bc0a1c77a8893b0e6092a87be37685843
Rahul8997 <rdprajapati8997@gmail.com>
2023-05-28 17:55:25 +0530

    Backend-Kickstarting Node Js-Task-9-Clean Up Code added

---
+++ BackEnd-Kickstarting Node Js-Task-9/app.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const requestHandler=require('./routes')
+
+const server = http.createServer(requestHandler.requestHandler)
+console.log(requestHandler.someText)
+
+server.listen(4000);
