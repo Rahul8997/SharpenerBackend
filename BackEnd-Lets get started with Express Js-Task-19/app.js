5e325316aced9d47c81134176c88a2b660e2097d
Rahul8997 <rdprajapati8997@gmail.com>
2023-06-23 01:37:04 +0530

    Backend-Lets get started with Express Js-Task-19-Full stack Expense App added

---
+++ BackEnd-Lets get started with Express Js-Task-19/app.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
+const sequelize = require('./util/database');
+
+var cors = require('cors');
+
+const app = express();
+
+
+app.use(cors());
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const userRoutes = require('./routes/user');
+const expenseRoutes = require('./routes/expense')
+
+app.use(bodyParser.json({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(userRoutes);
+app.use(expenseRoutes);
+app.use(errorController.get404);
+
+sequelize
+ .sync()
+ .then(()=> {
+    app.listen(3000);
+ })
+ .catch(err => {
+    console.log(err);
+ })
+
+
