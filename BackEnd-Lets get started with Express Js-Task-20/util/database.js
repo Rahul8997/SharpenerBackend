30dfdfb418ff4e5a3e29b91dd83d6a41baaf616f
Rahul8997 <rdprajapati8997@gmail.com>
2023-06-23 18:22:38 +0530

    Backend-Lets get started with Express Js-Task-20-Design the User and Product relationship added

---
+++ BackEnd-Lets get started with Express Js-Task-20/util/database.js
@@ -0,0 +1,8 @@
+const {Sequelize}=require('sequelize');
+
+const sequelize=new Sequelize('nodecomplete','root','Fzopp@8997e',{
+    dialect:'mysql',
+    host:'localhost'
+});
+
+module.exports=sequelize;
